27bb09bf511aea40896a8c8c7cf18b89f8b6ad64
Gal Malobani <gal.malobani@post.runi.ac.il>
2025-11-03 23:43:57 +0200

    Remove broken submodule

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,7 +3,7 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		//מקרה צהריים
+	
 		if(hours >= 12) {
 			if(hours>12) {
 				hours = hours - 12;
